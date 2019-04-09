253de9c5646436486e1880cec9631e828b7b75a8
Jody Garnett <jody.garnett@gmail.com>
2019-04-09 13:07:29 -0700

    Update or provide file headers
    Signed-off-by: Jody Garnett <jody.garnett@gmail.com>

--- modules/core/src/main/java/org/locationtech/rpe/Interpolation.java
+++ modules/core/src/main/java/org/locationtech/rpe/Interpolation.java
@@ -1,3 +1,10 @@
+/* Copyright (c) 2018 Jody Garnett and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, Version 2.0
+ * which accompanies this distribution and is available at
+ * http://www.opensource.org/licenses/apache2.0.php.
+ */
 package org.locationtech.rpe;
 
 public enum Interpolation implements KeyedHint {
