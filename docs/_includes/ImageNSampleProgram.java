204231301c4bc866845c37ebd3309472ac6607f4
Jody Garnett <jody.garnett@gmail.com>
2019-05-09 00:19:05 -0700

    Outline of docs with single code example
    Signed-off-by: Jody Garnett <jody.garnett@gmail.com>

---
+++ docs/_includes/ImageNSampleProgram.java
@@ -0,0 +1,70 @@
+import java.awt.Frame;
+import java.awt.image.renderable.ParameterBlock;
+import java.io.IOException;
+import org.eclipse.imagen.Interpolation;
+import org.eclipse.imagen.JAI;
+import org.eclipse.imagen.RenderedOp;
+import org.eclipse.imagen.codec.FileSeekableStream;
+import org.eclipse.imagen.widget.ScrollingImagePanel;
+/**
+ * This program decodes an image file of any supported
+ * formats, such as GIF, JPEG, TIFF, BMP, PNM, PNG, into a
+ * RenderedImage, scales the image by 2X with bilinear
+ * interpolation, and then displays the result of the scale
+ * operation.
+ */
+public class ImageNSampleProgram {
+    /** The main method. */
+    public static void main(String[] args) {
+        /* Validate input. */
+        if (args.length != 1) {
+            System.out.println("Usage: java ImageNSampleProgram " +
+                               "input_image_filename");
+            System.exit(-1);
+        }
+        /*
+         * Create an input stream from the specified file name
+         * to be used with the file decoding operator.
+         */
+        FileSeekableStream stream = null;
+        try {
+            stream = new FileSeekableStream(args[0]);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+        /* Create an operator to decode the image file. */
+        RenderedOp image1 = JAI.create("stream", stream);
+        /*
+         * Create a standard bilinear interpolation object to be
+         * used with the "scale" operator.
+         */
+        Interpolation interp = Interpolation.getInstance(
+                                   Interpolation.INTERP_BILINEAR);
+        /**
+         * Stores the required input source and parameters in a
+         * ParameterBlock to be sent to the operation registry,
+         * and eventually to the "scale" operator.
+         */
+        ParameterBlock params = new ParameterBlock();
+        params.addSource(image1);
+        params.add(2.0F);         // x scale factor
+        params.add(2.0F);         // y scale factor
+        params.add(0.0F);         // x translate
+        params.add(0.0F);         // y translate
+        params.add(interp);       // interpolation method
+        /* Create an operator to scale image1. */
+        RenderedOp image2 = JAI.create("scale", params);
+        /* Get the width and height of image2. */
+        int width = image2.getWidth();
+        int height = image2.getHeight();
+        /* Attach image2 to a scrolling panel to be displayed. */
+        ScrollingImagePanel panel = new ScrollingImagePanel(
+                                        image2, width, height);
+        /* Create a frame to contain the panel. */
+        Frame window = new Frame("JAI Sample Program");
+        window.add(panel);
+        window.pack();
+        window.show();
+    }
+}
---
+++ docs/index.md
@@ -0,0 +1,15 @@
+---
+layout: default
+title: Eclipse ImageN
+---
+# Eclipse ImageN
+
+## A simple ImageN Program
+
+Before proceeding any further, let's take a look at an example ImageN program to get an idea of what it looks like. The Listing below shows a simple example of a complete JAI program. This example reads an image, passed to the program as a command line argument, scales the image by 2x with bilinear interpolation, then displays the result.
+
+**Simple Example JAI Program**
+
+{% highlight java linenos%}
+{% include ImageNSampleProgram.java %}
+{% endhighlight %}
